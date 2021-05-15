29103f69dd1e353c856d19d8d53ccf2de32e4004
Sergio <vasiliokas@gmail.com>
2021-05-15 18:00:57 +0100

    Component

---
+++ Part 08 - Component/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace SergioBasic\Test;
+
+class Test 
+{
+    public function getSomething()
+    {
+        return "Getting something good";
+    }
+}
+
+// WIll get phpUnit
+// composere require phpunit/phpunit --dev
